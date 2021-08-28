161e98cbef95847a448136690be3187f479368b4
Anyzooka <80199862+anyzooka1@users.noreply.github.com>
2021-08-28 17:01:26 +0100

    T&Cs

---
+++ js/navBar.js
@@ -0,0 +1,10 @@
+var navBar = `
+<div class="menuBar">
+    <a href="index.html" class="menuButton"><button>Home page</button></a>
+    <a href="downloads.html" class="menuButton"><button>Downloads</button></a>
+    <a href="suggest.html" class="menuButton"><button>Suggestions</button></a>
+    <a href="T&C.html" class="menuButton"><button>Terms and Conditions</button></a>
+</div>
+`
+
+document.write(navBar);
