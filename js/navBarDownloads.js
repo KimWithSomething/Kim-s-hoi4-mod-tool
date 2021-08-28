809bcd137d32b638a245a742da168c3fb85cbeff
Anyzooka <80199862+anyzooka1@users.noreply.github.com>
2021-08-28 20:47:52 +0100

    a little bit of css and some animations

---
+++ js/navBarDownloads.js
@@ -0,0 +1,25 @@
+var navBar = `
+<div class="menuBar">
+    <table>
+        <tr>
+            <td>
+                <img height=40; width=auto style="padding-top:5px; padding-left:5px;" src="../images/logo.png" alt="logo">
+            </td>
+            <td>
+                <a href="../index.html" class="menuButton"><button>Home page</button></a>
+            </td>
+            <td>
+                <a href="../downloads.html" class="menuButton"><button>Downloads</button></a>
+            </td>
+            <td>
+                <a href="../suggest.html" class="menuButton"><button>Suggestions</button></a>
+            </td>
+            <td>
+                <a href="../T&C.html" class="menuButton"><button>Terms and Conditions</button></a>
+            </td>
+        </tr>
+    </table>
+</div>
+`
+
+document.write(navBar);
