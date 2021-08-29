7a4c5dc6bef188547a674b0b1adf05355a4b0224
Anyzooka <80199862+anyzooka1@users.noreply.github.com>
2021-08-29 11:12:30 +0100

    for some reason I have to upload the latest version

--- js/navBar.js
+++ js/navBar.js
@@ -6,19 +6,22 @@ var navBar = `
                 <img height=40; width=auto style="padding-top:5px; padding-left:5px;" src="images/logo.png" alt="logo">
             </td>
             <td>
-                <a href="index.html" class="menuButton"><button>Home page</button></a>
+                <a href="index.html"><button class="menuButton">Home page</button></a>
             </td>
             <td>
-                <a href="downloads.html" class="menuButton"><button>Downloads</button></a>
+                <a href="downloads.html"><button class="menuButton">Downloads</button></a>
             </td>
             <td>
-                <a href="suggest.html" class="menuButton"><button>Suggestions</button></a>
+                <a href="suggest.html"><button class="menuButton">Suggestions</button></a>
             </td>
             <td>
-                <a href="T&C.html" class="menuButton"><button>Terms and Conditions</button></a>
+                <a href="T&C.html"><button class="menuButton">Terms and Conditions</button></a>
             </td>
         </tr>
     </table>
+</div>
+<div class="credits">
+    <p>Website by anyzooka#3945</p>
 </div>
 `
 
